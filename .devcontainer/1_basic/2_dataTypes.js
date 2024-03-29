92d5b756dbc94bdb8884e3c2697c6e75b159ab40
AliBurhan11 <137722357+AliBurhan11@users.noreply.github.com>
2024-03-29 21:49:56 +0000

    dataTypes

---
+++ .devcontainer/1_basic/2_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3) // we are using nodejs not browser
+
+let name = "alee"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// primitivve dataTypes:
+// number => 2 to the power 53
+// bigint => 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object dataTypes:
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
