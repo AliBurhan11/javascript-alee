12e2568a5b576086e8c28a62ef4b7a358b57ec6b
AliBurhan11 <137722357+AliBurhan11@users.noreply.github.com>
2024-03-29 22:21:25 +0000

    converson of dataTypes

---
+++ .devcontainer/1_basic/3_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = true
+
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN - not a number
+// undefined => NaN
+// null => 0
+// true or false => 1 and 0 respectively
+
+let isLoggedIn = "alee"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// empty string "" => false; value added to string e.g "alee" => true
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
