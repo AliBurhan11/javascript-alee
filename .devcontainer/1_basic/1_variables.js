03760e1473baf67cb31566f18c83cdf116970f20
AliBurhan11 <137722357+AliBurhan11@users.noreply.github.com>
2024-03-29 21:00:45 +0000

    variables

---
+++ .devcontainer/1_basic/1_variables.js
@@ -0,0 +1,17 @@
+const accountId = 134433
+let accountEmail = "alee11@email.com"
+var accountPassword = "1234"
+accountCity = "kashmir"
+let accountState;
+
+/*
+accountId = 2 this is not allowed
+*/
+
+accountEmail = "alal@alal.com"
+accountPassword = "12121"
+accountCity = "Srinagar"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
