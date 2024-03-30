f42e3f84a5894248a398fe8127a9505d0dff87a8
AliBurhan11 <137722357+AliBurhan11@users.noreply.github.com>
2024-03-30 13:05:33 +0000

    convOperations

--- .devcontainer/1_basic/3_conversionOperations.js
+++ .devcontainer/1_basic/3_conversionOperations.js
@@ -22,5 +22,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 44
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************************. Operations. ********************************.
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello "
+let str2 = "alee"
+
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2);
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(true);
+// console.log(+ true);
+// console.log(3 + true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 3
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+let a = 4
+const y = a++;
+console.log(a, y);
+
+let b = 4
+const x = ++b;
+console.log(b, x);
