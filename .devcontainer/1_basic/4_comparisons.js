1a6041c780528a42d8e41f01a92895069934c6d8
AliBurhan11 <137722357+AliBurhan11@users.noreply.github.com>
2024-03-30 19:40:59 +0000

    comparison

---
+++ .devcontainer/1_basic/4_comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+// console.log("2" > 1);
+// console.log("02" >= 1);
+
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+// === comparison converstion it will check whether its a string, number, and not in ==
+// === if datatypes are different it wont convert "2" i.e a string to 1 i.e a number, but == will do it
+console.log(null === 0);
+console.log(null != 0);
+
+console.log(typeof (null));
+
+console.log(+null);
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
